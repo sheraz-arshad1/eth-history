b61ec71cd220055d96a2c0ad82041f4db96911fa
0xeth0x <sheraz2392@gmail.com>
2021-12-20 20:26:55 +0500

    introduce command line args for receving of accounts and add comments

--- alchemy-script.js
+++ alchemy-script.js
@@ -13,9 +13,15 @@ const INTERNAL = "internal";
 const TOKEN = "erc20";
 const DEFAULT_UUID = "DEFAULT_UUID";
 
+// Constants to differentiate between movement of assets.
 const ASSET_MOVEMENT = ["In", "Out"];
+
+// Constants to differentiate between type of assets.
 const ASSET_TYPES = ["ETH", "ERC20"];
 
+/**
+ * @dev Retrieves environment variable related to database connection.
+ */
 const {
     MARIADB_HOST,
     MARIADB_USER,
@@ -24,6 +30,10 @@ const {
     MARIADB_CONNECTION_LIMIT,
 } = process.env;
 
+/**
+ * @dev Establishes connection with database.
+ *
+ */
 const pool = mariadb.createPool({
     host: MARIADB_HOST,
     user: MARIADB_USER,
@@ -32,24 +42,50 @@ const pool = mariadb.createPool({
     connectionLimit: MARIADB_CONNECTION_LIMIT
 });
 
-main();
-
+/**
+ * @dev The main execution point of the script.
+ *
+ * It receives the list of addresses from command line arguments
+ * and take their corresponding synced block when their balances
+ * were last fetched and passes this data to {processAccounts}
+ * function to get the transfers against each provided account
+ * till the {latest} block.
+ *
+ * The retrieved transfers against list of addresses are stored in
+ * {Transfer} table in the database. The block that the balances are
+ * fetched till is stored in {UserSyncedBlock} table against the
+ * account.
+ */
 async function main() {
-    const accounts = [
-        "0x5e624faedc7aa381b574c3c2ff1731677dd2ee1d",
-        "0xaf648ffbc940570f3f6a9ca49b07ba7bc520bcdf",
-    ];
+    const accounts = process.argv.slice(2);
+    accounts.forEach(
+        (account) => {
+            if (!web3.utils.isAddress(account)) {
+                console.error("Invalid Address provided");
+                process.exit(1);
+            }
+        }
+    );
 
     const toBlock = await web3.eth.getBlockNumber();
     const toBlockHex = toHex(toBlock).toString();
     console.log(toBlock)
 
     const accountsAndLastFetchedBlocks = await getLastFetchedBlockForAccounts(accounts);
-    const data = await processAccounts(accountsAndLastFetchedBlocks, toBlockHex);
+    const data = await processAccounts(
+        accountsAndLastFetchedBlocks,
+        toBlockHex
+    );
     console.log(JSON.stringify(data))
     try {
+        // store all transfer data in database.
         await populateDatabase(data);
-        await updateLatestFetchedBlockForAccounts(accounts, toBlock);
+
+        // update the latest fetched for each of the accounts that are processed.
+        await updateLatestFetchedBlockForAccounts(
+            accounts,
+            toBlock
+        );
         process.exit(0);
     } catch (e) {
         console.error(e.message);
@@ -57,7 +93,22 @@ async function main() {
     }
 }
 
-async function processAccounts(accountsAndLastFetchedBlocks, toBlock) {
+/**
+ * @dev Iterates over all the provided accounts and call {fetchTransfers}
+ * to get the transfers against accounts. An account with large list of
+ * transfers requires multiple calls to {fetchTransfers} as the results
+ * returned by {alchemy_getAssetTransfers} API returns finite number of
+ * results and require pagination when the dataset is large.
+ *
+ * @param accountsAndLastFetchedBlocks An object represent each account
+ * and its corresponding last fetched block.
+ * @param toBlock The Block to fetch the transfers till.
+ * @returns {Promise<unknown>} Object containing fetched and constructed transfers.
+ */
+async function processAccounts(
+    accountsAndLastFetchedBlocks,
+    toBlock
+) {
     return Object.keys(accountsAndLastFetchedBlocks).reduce(
         async (
             acc,
@@ -142,7 +193,19 @@ async function processAccounts(accountsAndLastFetchedBlocks, toBlock) {
     );
 }
 
-function processTransfers(transfers, includeGasCost = false) {
+/**
+ * @dev Construct Transfers object specific to ERC20/ETH taking
+ * into account if the transfer results in gas cost for the user.
+ *
+ * @param transfers List of transfers (either ETH/ERC20).
+ * @param includeGasCost Are the transfers going out of account?
+ * if so, they accrue gas cost for the involved account.
+ * @returns {Promise} Returns transfer object.
+ */
+function processTransfers(
+    transfers,
+    includeGasCost = false
+) {
     return transfers.reduce(
         async (
             acc,
@@ -221,7 +284,11 @@ function processTransfers(transfers, includeGasCost = false) {
     );
 }
 
+/**
+ * @dev Generates payload to send to {alchemy_getAssetTransfers} API.
+ */
 function generatePayload(params) {
+    // {DEFAULT_UUID} means no pagination is needed.
     if (params.uuid === DEFAULT_UUID) delete params.uuid;
 
     return {
@@ -240,6 +307,9 @@ function generatePayload(params) {
     }
 }
 
+/**
+ * @dev Performs RPC call to web3 provider.
+ */
 async function fetchTransfers(params) {
     return new Promise((resolve, reject) => {
         web3.currentProvider.send(
@@ -253,6 +323,11 @@ async function fetchTransfers(params) {
     });
 }
 
+/**
+ * @dev Updates the last fetched block for user in the database.
+ * @param accounts List of involved accounts.
+ * @param lastFetchedBlockNum The block number to update with.
+ */
 async function updateLatestFetchedBlockForAccounts(
     accounts,
     lastFetchedBlockNum
@@ -271,6 +346,10 @@ async function updateLatestFetchedBlockForAccounts(
     await conn.end();
 }
 
+/**
+ * @dev Construct an object containing accounts and their last fetched blocks.
+ * @param accounts List of accounts
+ */
 async function getLastFetchedBlockForAccounts(accounts) {
     const conn = await pool.getConnection();
     const accountsAndLastFetchedBlocks = await accounts.reduce(
@@ -295,6 +374,12 @@ async function getLastFetchedBlockForAccounts(accounts) {
     return accountsAndLastFetchedBlocks;
 }
 
+/**
+ * @dev Construct an array by restructuring dataset to update database in batch
+ * instead of inserting each Transfer entry individually.
+ *
+ * @param data Dataset of all transfers constructed during the current run of script.
+ */
 function aggregateData(data) {
     return ASSET_MOVEMENT.reduce(
         (
@@ -343,6 +428,12 @@ function aggregateData(data) {
     );
 }
 
+/**
+ * @dev Updates the database with the constructed and fetched transfers data
+ * against provided list of accounts.
+ *
+ * @param data Aggregated dataset to insert into database.
+ */
 async function populateDatabase(data) {
     const conn = await pool.getConnection();
     await Object.keys(data).reduce(
@@ -372,3 +463,8 @@ async function populateDatabase(data) {
         }, Promise.resolve()
     );
 }
+
+/**
+ * @dev Starts the script.
+ */
+main();
